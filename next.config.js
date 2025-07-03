e32245ced0525e19c001a994696a32e8f1feb76b
hklfsyh <haikalafifsyah@gmail.com>
2025-07-03 17:39:44 +0700

    Deploy final version of new portfolio

--- next.config.js
+++ next.config.js
@@ -1,42 +1,17 @@
 /** @type {import('next').NextConfig} */
-const repo = 'hklfsyh.github.io'; 
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
 const nextConfig = {
+  // 1. Aktifkan static export
   output: 'export',
-  basePath: basePath,
-  assetPrefix: assetPrefix,
 
+  // 2. Nonaktifkan optimasi gambar untuk hosting statis
   images: {
-    unoptimized: true, 
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     remotePatterns: [
-      // Konfigurasi lama untuk placehold.co (tetap ada jika masih digunakan)
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      // Menambahkan domain baru untuk logo pendidikan
-      {
-        protocol: 'https',
-        hostname: 'learningcenter.mulaindonesia.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'psb.ccit-ftui.com',
-        port: '',
-        pathname: '/**',
-      },
+      { hostname: 'placehold.co' },
+      { hostname: 'learningcenter.mulaindonesia.com' },
+      { hostname: 'upload.wikimedia.org' },
+      { hostname: 'psb.ccit-ftui.com' },
     ],
   },
 };
