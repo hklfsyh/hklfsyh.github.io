67ff3f2b5b2a7376251ddf11407afa48ca744d46
hklfsyh <haikalafifsyah@gmail.com>
2025-07-03 18:06:24 +0700

    Deploy final version of new portfolio from folder out

--- src/app/components/AnimatedBackground.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/AnimatedBackground.tsx
-
-export default function AnimatedBackground() {
-  // Komponen ini akan dirender dengan style dari globals.css
-  return <div className="animated-background" />;
-}
